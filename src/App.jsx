5e677e56b4b01a52ee21b0de715548e94bce3d8b
Niki Powell <nhe.powell@gmail.com>
2024-03-29 15:08:04 -0500

    filtered for true false for told on joke

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,88 @@
+import { useEffect, useState } from "react"
 import "./App.css"
+import { getAllJokes, postNewJoke } from "./services/jokeServices.js"
+
+
 
 export const App = () => {
-  return <div>Hello World!</div>
+  const [inputValue, setInputValue] = useState([]) // store joke
+  const [allJokes, setAllJokes] = useState([]) // store all jokes
+  const [untoldJokes, setUntoldJoke] = useState([]) // store untold jokes
+  const [toldJokes, setToldJoke] = useState([]) //store told jokes
+
+  // get and store all of our jokes from the database
+  useEffect(() => {
+    getAllJokes().then(jokeArray => {
+      setAllJokes(jokeArray)
+    })
+  }, [inputValue])
+
+  useEffect(() => {
+    const untold = allJokes.filter(joke => joke.told === false)
+    setUntoldJoke(untold)
+  }, [allJokes])
+
+  useEffect(() => {
+    const told = allJokes.filter(joke => joke.told === true)
+    setToldJoke(told)
+  }, [allJokes])
+
+  // post the new joke with the input value and then clear the input field afterwards
+  const handleButtonClick = () => {
+    postNewJoke(inputValue)
+    setInputValue('')
+  }
+
+
+  return (
+    <div>
+      <header className="app-heading">
+          <h1 className="app-heading-text">Dad Jokes</h1>
+      </header>
+      <div className="joke-add-form">
+        <input
+            className="joke-input"
+            type="text"
+            placeholder="New One Liner"
+            value={inputValue}
+            onChange={(event) => {setInputValue(event.target.value)}}/>
+        <button className="joke-input-submit" onClick={handleButtonClick}>YAS</button>
+      </div>
+      <div className="joke-lists-container">
+        <div className="joke-list-container">
+          <h2>
+            Untold Jokes
+            <span></span>
+          </h2>
+          {untoldJokes.map(joke => {
+            return (
+              <section key={joke.id}>
+                  <div>
+                    <li className="joke-list-item">
+                      <p className="joke-list-item-text">{joke.text}</p>
+                    </li>
+                  </div>
+              </section>
+            )
+            }
+          )}
+        </div>
+        <div className="joke-list-container">
+        <h2>Told Jokes</h2>
+          {toldJokes.map(joke => {
+            return (
+              <section key={joke.id}>
+                <div>
+                  <li className="joke-list-item">
+                    <p className="joke-list-item-text">{joke.text}</p>
+                  </li>
+                </div>
+              </section>
+            )
+            }
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ src/services/jokeServices.js
@@ -0,0 +1,23 @@
+
+export const getAllJokes = () => {
+    return fetch('http://localhost:8088/jokes').then((res) => res.json())
+}
+
+
+export const postNewJoke = async (inputValue) => {
+
+    const newJokeObject = {
+        text: inputValue,
+        told: false
+    }
+
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newJokeObject)
+    }
+    await fetch('http://localhost:8088/jokes', postOptions)
+
+}
