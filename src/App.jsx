c12110c4b8ab15fb64667efbe40dd6a40261215c
Niki Powell <nhe.powell@gmail.com>
2024-04-01 14:01:29 -0500

    added counting capability, initial toggle and delete button elements with no functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ export const App = () => {
   const [allJokes, setAllJokes] = useState([]) // store all jokes
   const [untoldJokes, setUntoldJoke] = useState([]) // store untold jokes
   const [toldJokes, setToldJoke] = useState([]) //store told jokes
+  const [untoldJokeCount, setUntoldJokeCount] = useState(0) // store number untold jokes
+  const [toldJokeCount, settoldJokeCount] = useState(0) // store number untold jokes
+
+
 
   // get and store all of our jokes from the database
   useEffect(() => {
@@ -17,6 +21,9 @@ export const App = () => {
     })
   }, [inputValue])
 
+
+
+  // sort jokes as told or untold
   useEffect(() => {
     const untold = allJokes.filter(joke => joke.told === false)
     setUntoldJoke(untold)
@@ -27,11 +34,36 @@ export const App = () => {
     setToldJoke(told)
   }, [allJokes])
 
-  // post the new joke with the input value and then clear the input field afterwards
-  const handleButtonClick = () => {
-    postNewJoke(inputValue)
-    setInputValue('')
-  }
+
+
+  // count the number of each untold and told jokes, those values need to update with each post or page refresh
+  useEffect(() => {
+    const untoldJokeCount = untoldJokes.length
+    setUntoldJokeCount(untoldJokeCount)
+  }, [allJokes, untoldJokes])
+
+  useEffect(() => {
+    const toldJokeCount = toldJokes.length
+    settoldJokeCount(toldJokeCount)
+  }, [allJokes, toldJokes])
+
+
+
+    // post the new joke with the input value and then clear the input field afterwards
+    const handleButtonClick = () => {
+      postNewJoke(inputValue)
+      setInputValue('')
+      
+    }
+
+
+
+    // function for updated the database
+    const updateDatabase = () => {
+      getAllJokes().then((jokeArr) => {
+        setAllJokes(jokeArr)
+      })
+    }
 
 
   return (
@@ -50,16 +82,17 @@ export const App = () => {
       </div>
       <div className="joke-lists-container">
         <div className="joke-list-container">
-          <h2>
-            Untold Jokes
-            <span></span>
-          </h2>
+          <h2>Untold Jokes<span className="untold-count">{untoldJokeCount}</span></h2>
           {untoldJokes.map(joke => {
             return (
               <section key={joke.id}>
                   <div>
                     <li className="joke-list-item">
                       <p className="joke-list-item-text">{joke.text}</p>
+                      <span>
+                        <div><button className="joke-list-action-toggle"></button></div>
+                        <div><button className="joke-list-action-delete"></button></div>
+                      </span>
                     </li>
                   </div>
               </section>
@@ -68,13 +101,17 @@ export const App = () => {
           )}
         </div>
         <div className="joke-list-container">
-        <h2>Told Jokes</h2>
+        <h2>Told Jokes<span className="told-count">{toldJokeCount}</span></h2>
           {toldJokes.map(joke => {
             return (
               <section key={joke.id}>
                 <div>
                   <li className="joke-list-item">
                     <p className="joke-list-item-text">{joke.text}</p>
+                    <span>
+                        <div><button className="joke-list-action-toggle"></button></div>
+                        <div><button className="joke-list-action-delete"></button></div>
+                      </span>
                   </li>
                 </div>
               </section>
